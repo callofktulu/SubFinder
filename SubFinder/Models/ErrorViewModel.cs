6a7009334dce33d3aef92b7a9774150298724e03
tu-laptop <tugrulerturk@windowslive.com>
2017-09-18 17:26:51 +0200

    Add project files.

---
+++ SubFinder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SubFinder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
